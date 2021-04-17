95d858492f6f4ae36f854f6cd802103e7630c0ed
Pramudita Prabaswara <81899061+Pramuditapss@users.noreply.github.com>
2021-04-17 16:34:00 +0700

    Update hargatiket.java
    Commit Pramudita

--- tugas4/src/hargatiket.java
+++ tugas4/src/hargatiket.java
@@ -17,4 +17,52 @@ public class hargatiket {
         System.out.print("Berapa penumpang yang akan membeli tiket? : ");
         int penumpang = scan.nextInt();
 
+//Variabel hargasatuan nantinya akan digunakan untuk menghitung harga total tiket
+        int hargasatuan;
 
+        //Variabel back nantinya digunakan untuk kembali ke menu awal ketika input pengguna salah
+        String back = "y";
+
+        while (back=="y"){
+            pendefinisi.cetak1(" Solo", " Boyolali", " Salatiga");
+            System.out.print("Pilih tiket kereta yang akan anda beli    : ");
+            int try1 = scan.nextInt();
+            System.out.print("\n");
+            pendefinisi.cetak8();
+
+            //TIKET KE SOLO//
+            if (try1 == 1) {
+
+                //back = a menandakan bahwa program tidak akan diulang
+                back="a";
+
+                pendefinisi.cetak6("Solo");
+                pendefinisi.cetak8();
+
+                //Variabel back2 digunakan untuk kembali ke menu awal
+                String back2 ="z";
+
+                while(back2=="z"){
+                    pendefinisi.cetak3();
+                    int try2 = scan.nextInt();
+                    pendefinisi.cetak8();
+
+                    switch (try2) {
+                        case (1): hargasatuan = 100000;
+                            pendefinisi.cetak2(penumpang,hargasatuan);
+                            back2="a";
+                            break;
+                        case (2): hargasatuan = 150000;
+                            pendefinisi.cetak2(penumpang,hargasatuan);
+                            back2="a";
+                            break;
+                        case (3): hargasatuan = 200000;
+                            pendefinisi.cetak2(penumpang,hargasatuan);
+                            back2="a";
+                            break;
+                        default: pendefinisi.cetak5();
+                            back2="z";
+                            break;
+                    }
+                }
+            }
